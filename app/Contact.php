bfc40d0f52776cbd9e9b2a4b3857954fa9a3a4c0
Brenda Santos <brenda2santos@outlook.com>
2020-05-19 16:50:01 -0300

    helper/data treatment

--- app/Contact.php
+++ app/Contact.php
@@ -6,7 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    
     protected $fillable = [
         'salutation', 'name','number','email', 'birth','avatar','note'
     ];
+
+    public function getAvatarImageAttribute($value) {
+        return $this->avatar == null ? asset('images/null.png') : asset($this->avatar);
+    }
+    public function getAvatarFilenameAttribute($value) {
+        return substr($this->avatar, 30, strlen($this->avatar));
+    }
+    public function getBirthAttribute($value) {
+        return dateFormatDatabaseScreen($value, 'screen');
+    }
+
+    //mutator
+    public function setBirthAttribute($value) {
+        $this->attributes['birth'] = dateFormatDatabaseScreen($value);
+    }
+    public function setAvatarAttribute($value) {
+        $filename = substr(md5(rand(100000, 999999)),0,5) .'_'. $value->getClientOriginalName();
+        $filepath = 'public/uploads/'.date('Y').'/'.date('m').'/';
+        if ($value->isValid()) {
+            $path = $value->storeAs($filepath, $filename);
+        }
+        $this->attributes['avatar'] = str_replace('public', 'storage', $filepath) . $filename;
+    }
+
 }
